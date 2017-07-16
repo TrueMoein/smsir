735aed49f24d6b8c431748566f11380a48f073df
Moein Alizadeh <moein.alizade@gmail.com>
2017-07-16 12:24:33 +0430

    add some feature
    now you can set the paginate value in config file

--- src/config/smsir.php
+++ src/config/smsir.php
@@ -1,21 +1,24 @@
 <?php
 
-
-
 return [
 
 	/* Important Settings */
 
+	// ======================================================================
+	'middleware' => ['guest'],
 	// SMS.ir Api Key
-	'api-key' => env('SMSIR-API-KEY','Your sms.ir api key'),
+	'api-key' => env('SMSIR-API-KEY','Your api key'),
 	// SMS.ir Secret Key
-	'secret-key' => env('SMSIR-SECRET-KEY','Your sms.ir secret key'),
-
-	'line-number' => env('SMSIR-LINE-NUMBER','Your sms.ir line number'),
-
+	'secret-key' => env('SMSIR-SECRET-KEY','Your secret key'),
+	// Your sms.ir line number
+	'line-number' => env('SMSIR-LINE-NUMBER','Your Sms.ir Line Number'),
+	// ======================================================================
+	
+	// set true if you want log to the database
 	'db-log' => true,
-	/* Admin Panel */
 
-	'title' => 'مدیریت پیامک ها'
-
+	/* Admin Panel Title */
+	'title' => 'مدیریت پیامک ها',
+	// How many log you want to show in sms-admin panel ?
+	'in-page' => '15'
 ];
