fea475a89e99fc2c185a1109ec1b5bc02bb9644c
Moein Alizadeh <moein.alizade@gmail.com>
2017-07-16 12:26:06 +0430

    add new feature
    add sendStatus method. return status of log

--- src/models/SmsirLogs.php
+++ src/models/SmsirLogs.php
@@ -7,7 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 class SmsirLogs extends Model
 {
 	protected $guarded = [];
-
 	protected $table = 'smsir_logs';
 
+	public function sendStatus() {
+		if($this->status){
+			return '<i class="fa fa-check-circle" aria-hidden="true" style="color: green"></i>';
+		} else {
+			return '<i class="fa fa-exclamation-circle" aria-hidden="true" style="color: red"></i>';
+		}
+	}
 }
