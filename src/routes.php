094742528ce15bc583665e07835fea4bd9d4652a
Moein Alizadeh <moein.alizade@gmail.com>
2017-07-16 12:32:35 +0430

    update the route
    remove unnecessary routes

--- src/routes.php
+++ src/routes.php
@@ -1,11 +1,5 @@
 <?php
-
-
-Route::group(['namespace'=>'phplusir\smsir\Controllers'], function (){
-	Route::get('sms-admin','SmsirController@index');
-	Route::get('sms-admin/create','SmsirController@create');
-	Route::get('sms-admin/send','SmsirController@send');
-	Route::get('sms-admin/add','SmsirController@addToCustomer');
-	Route::get('sms-admin/sendcc','SmsirController@sendCC');
-	Route::get('sms-admin/verf','SmsirController@verf');
+Route::group(['namespace'=>'phplusir\smsir\Controllers','middleware'=>config('smsir.middleware')], function (){
+	Route::get('sms-admin','SmsirController@index')->name('sms-admin');
+	Route::get('sms-admin/{log}/delete','SmsirController@delete')->name('deleteLog');
 });
