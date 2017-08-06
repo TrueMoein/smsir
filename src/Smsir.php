3a9e5e8af562336f98c5b6f91668c9e0f318480d
Moein Alizadeh <moein.alizade@gmail.com>
2017-08-06 09:36:40 +0430

    update 1.4
    update package to 1.4

--- src/Smsir.php
+++ src/Smsir.php
@@ -13,7 +13,7 @@ class Smsir
 	public static function getToken()
 	{
 		$client     = new Client();
-		$body       = ['UserApiKey'=>config('smsir.api-key'),'SecretKey'=>config('smsir.secret-key'),'System'=>'laravel_v_1_1'];
+		$body       = ['UserApiKey'=>config('smsir.api-key'),'SecretKey'=>config('smsir.secret-key'),'System'=>'laravel_v_1_4'];
 		$result     = $client->post('http://ws.sms.ir/api/Token',['json'=>$body]);
 		return json_decode($result->getBody(),true)['TokenKey'];
 	}
