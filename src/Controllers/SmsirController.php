84eda065181bf71d54dd5aca8187f14dac54e44f
mohammad-y <30681568+mohammad-y@users.noreply.github.com>
2017-11-09 19:19:41 +0330

    Update SmsirController.php
    مرتب سازی کد ها
    
    اعمال تغییر مربوط به namespace SmsirLogs
    
    بهینه سازی تابع حذف لاگ ها

--- src/Controllers/SmsirController.php
+++ src/Controllers/SmsirController.php
@@ -1,26 +1,29 @@
 <?php
 
 namespace phplusir\smsir\Controllers;
+
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Route;
+use phplusir\smsir\models\SmsirLogs;
 use phplusir\smsir\Smsir;
-use phplusir\smsir\SmsirLogs;
 
 
 class SmsirController extends Controller
 {
 
-	// the main index page for administrators
-	public function index() {
-		$credit = Smsir::credit();
-		$smsir_logs = SmsirLogs::orderBy('id','DESC')->paginate(config('smsir.in-page'));
-		return view('smsir::index',compact('credit','smsir_logs'));
-	}
+    // the main index page for administrators
+    public function index()
+    {
+        $credit = Smsir::credit();
+        $smsir_logs = SmsirLogs::orderBy('id', 'DESC')->paginate(config('smsir.in-page'));
+        return view('smsir::index', compact('credit', 'smsir_logs'));
+    }
 
-	// administrators can delete single log
-	public function delete() {
-		SmsirLogs::where('id',Route::current()->parameters['log'])->delete();
-		// return the user back to sms-admin after delete the log
-		return back();
-	}
+    // administrators can delete single log
+    public function delete(SmsirLogs $log)
+    {
+        $log->delete();
+        // return the user back to sms-admin after delete the log
+        return back();
+    }
 }
